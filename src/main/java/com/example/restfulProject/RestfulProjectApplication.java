8c490a837b5f859e223e09256fc925e103f069ff
Andrei <andreidumitrascuofc@gmail.com>
2023-01-24 11:25:51 -0500

    Initial commit

---
+++ src/main/java/com/example/restfulProject/RestfulProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restfulProject/RestfulProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
